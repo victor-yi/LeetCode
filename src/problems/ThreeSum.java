b1cc95ff3e1ec358738acdad7ea938043c10917e
viktoryi <viktoryi@163.com>
2018-11-17 01:02:47 +0800

    commit the solution for problem 15

---
+++ src/problems/ThreeSum.java
@@ -0,0 +1,54 @@
+package problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-1, 0, 1, 2, -1, -4};
+//        int[] array = new int[]{0, 0, 0, 0};
+//        int[] array = new int[]{-2, 0, 1, 1, 2};
+        System.out.println(threeSum(array));
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i - 1 >= 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            left = i + 1;
+            right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left + 1 < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right - 1 > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+}
