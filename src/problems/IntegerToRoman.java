6473aa71de55d3104b61e786da12e7418191705f
Victor Jun Yi <v-vyi@expedia.com>
2018-11-16 22:30:20 +0800

    update the code

--- src/problems/IntegerToRoman.java
+++ src/problems/IntegerToRoman.java
@@ -14,42 +14,4 @@ public class IntegerToRoman {
         return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
 
-    public static String intToRoman(int num) {
-
-        char[] chars = new char[]{'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-        StringBuilder sb = new StringBuilder();
-        int quotient = num / 10;
-        int remainder = num % 10;
-        int level = 0;
-
-        while (quotient > 0 || remainder > 0) {
-            StringBuilder tmp = new StringBuilder();
-            if (remainder == 9) {
-                tmp.append(chars[level]);
-                tmp.insert(0, chars[level + 2]);
-            } else if (remainder > 4) {
-                tmp.append(chars[level + 1]);
-                for (int i = 0; i < remainder % 5; i++) {
-                    tmp.insert(0, chars[level]);
-                }
-            } else if (remainder == 4) {
-                tmp.append(chars[level]);
-                tmp.insert(0, chars[level + 1]);
-            } else {
-                for (int i = 0; i < remainder; i++) {
-                    tmp.append(chars[level]);
-                }
-            }
-
-            sb.append(tmp);
-            remainder = quotient % 10;
-            quotient = quotient / 10;
-            level += 2;
-        }
-
-        return sb.reverse().toString();
-    }
-
-
-
 }
