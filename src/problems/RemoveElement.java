b9ebed856faa1c9e53f11941061561b8b0fe0065
viktoryi <viktoryi@163.com>
2018-11-18 23:00:16 +0800

    commit solution for problem 27

---
+++ src/problems/RemoveElement.java
@@ -0,0 +1,23 @@
+package problems;
+
+public class RemoveElement {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(removeElement(array, 2));
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " . ");
+        }
+
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
