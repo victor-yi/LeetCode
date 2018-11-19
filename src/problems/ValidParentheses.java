b449f7b0253e3b8cf4e478ba5f76e5fc8ce88d5f
viktoryi <viktoryi@163.com>
2018-11-19 18:27:08 +0800

    commit the solution for problem 20

---
+++ src/problems/ValidParentheses.java
@@ -0,0 +1,37 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        String str = "{{[]()}}";
+//        String str = "{{[](})}";
+        System.out.println(isValid(str));
+    }
+
+    public static boolean isValid(String s) {
+        HashMap<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (mapping.containsKey(c)) {
+                char top = !stack.empty() ? stack.pop() : '?';
+                if (top != mapping.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+
+}
