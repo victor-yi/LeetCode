95a0092fe330ddc730753df63a5cde4ecd2b3896
viktoryi <viktoryi@163.com>
2018-11-22 10:17:31 +0800

    commit the solution for problem 26

---
+++ src/problems/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,27 @@
+package problems;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int count = removeDuplicates(array);
+        System.out.println(count);
+        for (int i = 0; i < count; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+
+            nums[count++] = nums[i];
+        }
+        return count;
+    }
+
+}
