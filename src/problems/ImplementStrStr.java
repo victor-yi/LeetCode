89c68f7542f0b4717fda3a38e95947b59d32778c
viktoryi <viktoryi@163.com>
2018-11-23 11:29:51 +0800

    commit the solution for problem 28

---
+++ src/problems/ImplementStrStr.java
@@ -0,0 +1,41 @@
+package problems;
+
+public class ImplementStrStr {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("ABCDE", "BC"));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+
+        int searchLength = haystack.length() - needle.length();
+        int searchIndex = 0;
+        char firstSearchChar = needle.charAt(searchIndex);
+        for (int i = 0; i <= searchLength; i++) {
+            if (haystack.charAt(i) != firstSearchChar) {
+                while (++i <= searchLength && haystack.charAt(i) != firstSearchChar) {
+
+                }
+            }
+
+            int nextSearchPosition = i + 1;
+            int currentMaxLength = nextSearchPosition + needle.length() - 1;
+
+            if (i <= searchLength) {
+                for (searchIndex = 1;
+                     nextSearchPosition < currentMaxLength && haystack.charAt(nextSearchPosition) == needle.charAt(searchIndex);
+                     nextSearchPosition++, searchIndex++) {
+                }
+
+                if (nextSearchPosition == currentMaxLength) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
