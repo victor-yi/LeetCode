2200102b74d660cf1325cb9032329472ee3da2dd
viktoryi <viktoryi@163.com>
2019-01-20 00:08:00 +0800

    commit the solution for problem 146

---
+++ src/problems/Q146_LRUCache.java
@@ -0,0 +1,41 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+public class Q146_LRUCache {
+
+    private int capacity;
+    private Map<Integer, Integer> cache;
+    private Queue<Integer> accessOrder;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.accessOrder = new LinkedList<>();
+    }
+    
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            accessOrder.remove(key);
+            accessOrder.add(key);
+            return cache.get(key);
+        } else {
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            accessOrder.remove(key);
+        } else if (cache.size() >= capacity) {
+            int lastKey = accessOrder.poll();
+            cache.remove(lastKey); 
+        } 
+        cache.put(key, value);
+        accessOrder.add(key);
+    }
+
+}
